b87be5a59f9d06d956868e442ff7d1eb84699997
timcrone <34396390+timcrone@users.noreply.github.com>
2017-12-09 08:07:23 -0500

    Create create-ten-user.sh

---
+++ create-ten-user.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+for ((i=1;i<10;i++)) do ddsh -a user add 123456789012345678901234567891$i password abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij ; done
